18e1f4839188906b50dc0dcb3255e4f6ac3bd889
Thomas Leskin <tleskin@gmail.com>
2015-08-11 10:37:58 -0600

    Add list to list index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,25 @@
 class ListsController < ApplicationController
   def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    list = List.new(list_params)
+    if list.save
+      redirect_to root_path
+    else
+      @list = List.new
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :list
   root 'lists#index'
+  resources :lists
 end
