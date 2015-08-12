cc171d6a26d66a6ed7ae05e215ccbc1ffe839fce
Thomas Leskin <tleskin@gmail.com>
2015-08-12 04:46:18 -0600

    Add Bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :all_lists
+
+  def all_lists
+    @lists ||= List.all
+  end
 end
